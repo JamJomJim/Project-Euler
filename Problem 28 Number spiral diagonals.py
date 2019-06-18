2ebe2729d292f5a567f9150cd84633e19de81fa4
JamJomJim <thejamesewens@gmail.com>
2019-06-17 21:50:04 -0500

    Problem 28 Number spiral diagonals

---
+++ Problem 28 Number spiral diagonals.py
@@ -0,0 +1,12 @@
+def answer():
+    total = 1
+    num = 1
+    increment = 2
+    while num < 1001**2:
+        for i in range(1, 5):
+            num += increment
+            total += num
+        increment += 2
+    print(total)
+
+answer()
